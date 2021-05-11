11aaaed7de481d53645691e91cc8b5dd43a69353
Paul McKibbin <pmckibbin@gmail.com>
2021-05-11 13:22:36 +0100

    Remove the default from item_id. As it's null: false, it must be being filled in on creation in any case by papertrail.

--- db/migrate/20210510130434_create_participant_events_papertrail_versioning_table.rb
+++ db/migrate/20210510130434_create_participant_events_papertrail_versioning_table.rb
@@ -9,7 +9,7 @@ class CreateParticipantEvents < ActiveRecord::Migration[6.1]
       t.json :object
       t.json :object_changes
       t.datetime :created_at
-      t.uuid :item_id, default: -> { "gen_random_uuid()" }, null: false
+      t.uuid :item_id, null: false
       t.index %i[item_type item_id]
     end
   end
